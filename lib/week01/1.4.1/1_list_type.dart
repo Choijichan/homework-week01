f17662003138945764073b9269c5087f3198ed9c
choijichan <sjy08007@gmail.com>
2025-09-07 01:39:17 +0900

    feat(week01): Demonstrate list length and element modification

--- lib/week01/1.4.1/1_list_type.dart
+++ lib/week01/1.4.1/1_list_type.dart
@@ -5,4 +5,8 @@ void main() {
   print(blackPinkList);
   print(blackPinkList[0]); // 첫 원소 지정
   print(blackPinkList[3]); // 마지막 원소 지정
+
+  print(blackPinkList.length); // 길이 반환
+
+  blackPinkList[3] = '코드팩토리'; // 3번 인덱스값 변경
 }
