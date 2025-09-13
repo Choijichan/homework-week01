43286cefe783eaa8f67640cbcff15ae81fee1d2d
choijichan <sjy08007@gmail.com>
2025-09-13 16:11:45 +0900

    feat: Implement basic inheritance example in Idol class

--- lib/week01/2.3/1_inheritance.dart
+++ lib/week01/2.3/1_inheritance.dart
@@ -12,3 +12,13 @@ class Idol {
     print('${this.name} 멤버는 ${this.membersCount}명입니다.');
   }
 }
+// extends 키워드를 사용해서 상속받습니다.
+// class 자식 클래스 extends 부모 클래스 순서입니다.
+class BoyGroup extends Idol {
+
+  // 상속받은 생성자
+  BoyGroup(
+    String name,
+    int membersCount,
+  ) : super(  // super는 부모 클래스를 지칭합니다.)
+}
