280a71f615640edbb4e51a7b6a8b8707e5b611a4
choijichan <sjy08007@gmail.com>
2025-09-13 16:42:02 +0900

    feat: Add Idol class with name and member count

--- lib/week02/2.6/1_mixin.dart
+++ lib/week02/2.6/1_mixin.dart
@@ -1,3 +1,18 @@
+class Idol {
+  final String name;
+  final int membersCount;
+
+  Idol(this.name, this.membersCount);
+
+  void sayName() {
+    print('저는 ${this.name}입니다.');
+  }
+
+  void sayMembersCount() {
+    print('${this.name} 멤버는 ${this.membersCount}명입니다.');
+  }
+}
+
 mixin IdolSingMixin on Idol {
   void sing() {
     print('${this.name}이 노래를 부릅니다.');
