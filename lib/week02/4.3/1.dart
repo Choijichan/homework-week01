04cabcdb4d8e2e39b615ba1708fe66e42218e3a2
choijichan <sjy08007@gmail.com>
2025-09-13 17:48:34 +0900

    feat(week02/4.3): Add switch expression cases for all days of the week

--- lib/week02/4.3/1.dart
+++ lib/week02/4.3/1.dart
@@ -5,6 +5,12 @@ void main() {
   String dayEnglish = switch (dayKor) {
     // '=>'를 사용하면 switch문 조건에 맞을 때 값을 반환할 수 있습니다.
     '월요일' => 'Monday',
-    ''
+    '화요일' => 'Tuesday',
+    '수요일' => 'Wednesday',
+    '목요일' => 'Thursday',
+    '금요일' => 'Friday',
+    '토요일' => 'Saturday',
+    '일요일' => 'Sunday',
+    // 
   }
 }
