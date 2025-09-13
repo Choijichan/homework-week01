3f6964e377d7e1a3428afa2775c04ecf60ed3ec2
choijichan <sjy08007@gmail.com>
2025-09-13 16:41:32 +0900

    feat: Implement IdolSingMixin and BoyGroup class

---
+++ lib/week02/2.6/1_mixin.dart
@@ -0,0 +1,10 @@
+mixin IdolSingMixin on Idol {
+  void sing() {
+    print('${this.name}이 노래를 부릅니다.');
+  }
+}
+
+// 믹스인을 적용할 때는 with 키워드 사용
+class BoyGroup extends Idol with IdolSingMixin {
+  BoyGroup(super.name, super.membersCount);
+}
