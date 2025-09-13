2609999ce55930473e172c38a3464daf888cad30
choijichan <sjy08007@gmail.com>
2025-09-13 16:43:32 +0900

    feat(mixin): Add sayName method to Idol class

--- lib/week02/2.6/1_mixin.dart
+++ lib/week02/2.6/1_mixin.dart
@@ -1,6 +1,12 @@
 class Idol {
   final String name;
   final int membersCount;
+
+  Idol(this.name, this.membersCount);
+
+  void sayName() {
+    print('저는 ${this.name}입니다.');
+  }
 }
 
 mixin IdolSingMixin on Idol {
