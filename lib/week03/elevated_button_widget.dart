b5cb8faa4ec308993004292e61a8ba8c712aac14
choijichan <sjy08007@gmail.com>
2025-09-19 11:49:57 +0900

    feat: Implement basic ElevatedButton widget with red background

---
+++ lib/week03/elevated_button_widget.dart
@@ -0,0 +1,10 @@
+ElevatedButton(
+  // 클릭 시 실행할 함수
+  onPressed: (){},
+  // 버튼 스타일링
+  style: ElevatedButton.styleFrom(
+    backgroundColor: Colors.red,
+  ),
+  // 버튼에 들어갈 위젯
+  child: Text('엘리베이티드 버튼'),
+),
