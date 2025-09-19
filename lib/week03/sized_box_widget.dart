ab523a1557e2e86f88917e9b4ecf4afc7008bd37
choijichan <sjy08007@gmail.com>
2025-09-19 12:42:58 +0900

    Fix: Correct SizedBox widget formatting in week03

--- lib/week03/sized_box_widget.dart
+++ lib/week03/sized_box_widget.dart
@@ -11,15 +11,15 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child:SizedBox(
-  // 높이 지정
-  height: 200.0,
+            // 높이 지정
+            height: 200.0,
 
-  // 너비 지정
-  width: 200.0,
+            // 너비 지정
+            width: 200.0,
 
-  // SizedBox는 색상이 없으므로 크기를 확인하는
-  // 용도로 Container 추가
-  child: Container(
-    color: Colors.red,
-  ),
-)
+            // SizedBox는 색상이 없으므로 크기를 확인하는
+            // 용도로 Container 추가
+            child: Container(
+              color: Colors.red,
+            ),
+          )
