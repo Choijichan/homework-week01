ae86fb9e0c8292511e07717cabdd7a38eed16e5b
choijichan <sjy08007@gmail.com>
2025-09-19 12:33:28 +0900

    feat: Implement basic SizedBox example

--- lib/week03/sized_box_widget.dart
+++ lib/week03/sized_box_widget.dart
@@ -1,5 +1,19 @@
+import'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body
+      )
+    )
+  }
+}
 SizedBox(
   // 높이 지정
   height: 200.0,
