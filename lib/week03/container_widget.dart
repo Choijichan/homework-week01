3841b0ec22e7ceb6a0f439c10633e11f7d855d77
choijichan <sjy08007@gmail.com>
2025-09-19 12:25:58 +0900

    feat: Remove styling from Container widget example

--- lib/week03/container_widget.dart
+++ lib/week03/container_widget.dart
@@ -32,24 +32,3 @@ class MyApp extends StatelessWidget {
   // 너비
   width: 100.0,
 )
-  // 스타일 작용
-  decoration: BoxDecoration(
-    // 배경색 적용
-    color: Colors.red,
-    // 테두리 적용
-    border: Border.all(
-      // 테두리 굵기
-      width: 16.0,
-      // 테두리 색상
-      color: Colors.black,
-    ),
-    // 모서리 둥글게 만들기
-    borderRadius: BorderRadius.circular(
-      16.0,
-    ),
-  ),
-  // 높이
-  height: 200.0,
-  // 너비
-  width: 100.0,
-)
