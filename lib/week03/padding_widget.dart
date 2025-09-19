e22fa3a485b2232b7d71ffadba61ea8643ba76e8
choijichan <sjy08007@gmail.com>
2025-09-19 12:45:58 +0900

    Fix: Wrap Padding Widget in MaterialApp and Scaffold for proper display.

--- lib/week03/padding_widget.dart
+++ lib/week03/padding_widget.dart
@@ -1,18 +1,27 @@
 import'package:flutter/material.dart'
 
-void main
-Container(
-  color: Colors.blue,
-  child: Pagging(
+void main() {
+  runApp(MyApp());
+}
 
-    // 상하, 좌우로 모두 16픽셀만큼 패딩 적용
-    padding: EdgeInsets.all(
-      16.0,
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child:Container(
+    color: Colors.blue,
+    child: Pagging(
+
+      // 상하, 좌우로 모두 16픽셀만큼 패딩 적용
+      padding: EdgeInsets.all(
+        16.0,
+      ),
+      child: Container(
+        color: Colors.red,
+        width: 50.0,
+        height: 50.0,
+      ),
     ),
-    child: Container(
-      color: Colors.red,
-      width: 50.0,
-      height: 50.0,
-    ),
-  ),
-)
+  )
