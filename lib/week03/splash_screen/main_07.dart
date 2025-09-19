012cf395c751165523714a4d8961c4050fc2222f
choijichan <sjy08007@gmail.com>
2025-09-19 14:00:29 +0900

    feat: Implement basic splash screen UI

---
+++ lib/week03/splash_screen/main_07.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFFF99231),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                width: 200, // 너비 추가
+              ),
+              CircuarProgressIndicator(),
+            ]
+          )
+        )
+      )
+    )
+  }
+}
