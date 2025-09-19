160847d5a9b73fa069313838ec50c44fabe0e073
choijichan <sjy08007@gmail.com>
2025-09-19 12:29:58 +0900

    Fix: Apply proper formatting to Container widget

--- lib/week03/container_widget.dart
+++ lib/week03/container_widget.dart
@@ -11,24 +11,24 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Container(
-  // 스타일 작용
-  decoration: BoxDecoration(
-    // 배경색 적용
-    color: Colors.red,
-    // 테두리 적용
-    border: Border.all(
-      // 테두리 굵기
-      width: 16.0,
-      // 테두리 색상
-      color: Colors.black,
-    ),
-    // 모서리 둥글게 만들기
-    borderRadius: BorderRadius.circular(
-      16.0,
-    ),
-  ),
-  // 높이
-  height: 200.0,
-  // 너비
-  width: 100.0,
-)
+              // 스타일 작용
+            decoration: BoxDecoration(
+              // 배경색 적용
+              color: Colors.red,
+              // 테두리 적용
+              border: Border.all(
+              // 테두리 굵기
+                width: 16.0,
+              // 테두리 색상
+            color: Colors.black,
+          ),
+          // 모서리 둥글게 만들기
+          borderRadius: BorderRadius.circular(
+            16.0,
+          ),
+        ),
+        // 높이
+        height: 200.0,
+        // 너비
+        width: 100.0,
+      )
