25e4fc4f65ede6ec9027481c0867fba7e242870c
choijichan <sjy08007@gmail.com>
2025-09-19 12:46:28 +0900

    feat: Add padding widget example with nested container

--- lib/week03/padding_widget.dart
+++ lib/week03/padding_widget.dart
@@ -11,17 +11,22 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child:Container(
-    color: Colors.blue,
-    child: Pagging(
+            color: Colors.blue,
+            child: Pagging(
 
-      // 상하, 좌우로 모두 16픽셀만큼 패딩 적용
-      padding: EdgeInsets.all(
-        16.0,
+              // 상하, 좌우로 모두 16픽셀만큼 패딩 적용
+              padding: EdgeInsets.all(
+                16.0,
+              ),
+              child: Container(
+                color: Colors.red,
+                width: 50.0,
+                height: 50.0,
+              ),
+            ),
+          ),
+        ),
       ),
-      child: Container(
-        color: Colors.red,
-        width: 50.0,
-        height: 50.0,
-      ),
-    ),
-  )
+    );
+  }
+}
