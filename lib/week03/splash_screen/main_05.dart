9992c70affd5f255bd1a642d70e624a5a104d668
choijichan <sjy08007@gmail.com>
2025-09-19 13:52:29 +0900

    feat(splash): Implement basic splash screen UI

--- lib/week03/splash_screen/main_05.dart
+++ lib/week03/splash_screen/main_05.dart
@@ -4,4 +4,25 @@ void main() {
   runApp(SplashScreen());
 }
 
-class
+class SpalshScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFFF99231),
+          ),
+          child: Column
+          children: [
+            // 여러 위젯을 입력할 수 있는 children 매개변수
+              Image.asset(
+                'assets/logo.png',
+              ),
+              CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
