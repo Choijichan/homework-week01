d60ab1b7bb3feb13c7cae05454a5a75f3e44cf09
choijichan <sjy08007@gmail.com>
2025-09-19 11:44:27 +0900

    feat: Add basic TextButton widget to week03

---
+++ lib/week03/text_button_widget.dart
@@ -0,0 +1,11 @@
+TextButton(
+  // 클릭 시 실행
+  onPressed: () {},
+  // 스타일 지정
+  style: TextButton.styleFrom(
+    // 주색상 지정
+    foregroundColor: Colors.red,
+  ),
+  // 버튼에 넣을 위젯
+  child: Text('텍스트 버튼'),
+)
