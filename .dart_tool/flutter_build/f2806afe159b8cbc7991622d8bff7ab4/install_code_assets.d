543e6837c2893e970237513d933df37e5b3652eb
choijichan <sjy08007@gmail.com>
2025-09-19 11:59:58 +0900

    feat: Add Scaffold to FloatingActionButtonExample

---
+++ .dart_tool/flutter_build/f2806afe159b8cbc7991622d8bff7ab4/install_code_assets.d
@@ -0,0 +1,1 @@
+ C:\\Users\\sjy08\\FlutterProject\\homework-week01\\.dart_tool\\flutter_build\\f2806afe159b8cbc7991622d8bff7ab4\\native_assets.json: 
--- lib/week03/floating_action_button_widget.dart
+++ lib/week03/floating_action_button_widget.dart
@@ -8,12 +8,14 @@ class FloatingActionButtonExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      floatingActionButton: FloatingActionButton(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
         // 클릭했을 때 실행할 함수
-        onPressed: () {},
-        child: Text('클릭'),
+          onPressed: () {},
+          child: Text('클릭'),
+        ),
+        body: Container(),
       ),
-      body: Container(),
-    ),
+    );
   }
 }
