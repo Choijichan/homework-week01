5c1c8de357ce605c64549f377ed1633db76f272d
choijichan <sjy08007@gmail.com>
2025-09-19 12:49:28 +0900

    feat: Add TextButton widget example

--- lib/week03/text_button_widget.dart
+++ lib/week03/text_button_widget.dart
@@ -11,13 +11,18 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child:TextButton(
-          // 클릭 시 실행
-          onPressed: () {},
-          // 스타일 지정
-          style: TextButton.styleFrom(
-            // 주색상 지정
-            foregroundColor: Colors.red,
+            // 클릭 시 실행
+            onPressed: () {},
+            // 스타일 지정
+            style: TextButton.styleFrom(
+              // 주색상 지정
+              foregroundColor: Colors.red,
+            ),
+            // 버튼에 넣을 위젯
+            child: Text('텍스트 버튼'),
           ),
-          // 버튼에 넣을 위젯
-          child: Text('텍스트 버튼'),
-        )
+        ),
+      ),
+    );
+  }
+}
