b64fee435064b21857d4620c74929554f5dbeced
choijichan <sjy08007@gmail.com>
2025-09-19 13:49:29 +0900

    feat: Implement basic splash screen UI with logo

--- lib/week03/splash_screen/main_04.dart
+++ lib/week03/splash_screen/main_04.dart
@@ -4,4 +4,23 @@ void main() {
   runApp(SplashScreen());
 }
 
-class SplashScreen extends StatelessWidget {}
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.orange,
+          ),
+          child: Center(
+            // Text 위젯을 Image 위젯으로 변경
+            child: Image.asset(
+              'assets/logo.png',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
