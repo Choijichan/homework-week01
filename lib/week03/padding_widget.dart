21229d5a39f2b873b05effa6fce53dae51a9b5ff
choijichan <sjy08007@gmail.com>
2025-09-19 12:11:58 +0900

    feat: Implement padding widget with EdgeInsets.all

---
+++ lib/week03/padding_widget.dart
@@ -0,0 +1,15 @@
+Container(
+  color: Colors.blue,
+  child: Pagging(
+
+    // 상하, 좌우로 모두 16픽셀만큼 패딩 적용
+    padding: EdgeInsets.all(
+      16.0,
+    ),
+    child: Container(
+      color: Colors.red,
+      width: 50.0,
+      height: 50.0,
+    ),
+  ),
+)
