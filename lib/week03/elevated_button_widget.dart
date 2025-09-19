45c19d61d6bd1a6051f0c38b58d2647bcfc5b488
choijichan <sjy08007@gmail.com>
2025-09-19 12:58:29 +0900

    feat: Implement basic ElevatedButton widget with styling

--- lib/week03/elevated_button_widget.dart
+++ lib/week03/elevated_button_widget.dart
@@ -3,7 +3,14 @@ import'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-ElevatedButton(
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child:ElevatedButton(
   // 클릭 시 실행할 함수
   onPressed: (){},
   // 버튼 스타일링
