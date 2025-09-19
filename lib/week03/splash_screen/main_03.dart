5c94d485f8c65fa616c5effb88ef845e89aec096
choijichan <sjy08007@gmail.com>
2025-09-19 13:41:59 +0900

    feat: Implement basic splash screen UI

--- lib/week03/splash_screen/main_03.dart
+++ lib/week03/splash_screen/main_03.dart
@@ -6,4 +6,19 @@ void main() {
 
 class SplashScreen extends StatelessWidget {
   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(  // 컨테이너 위젯
+          // 컨테이너를 디자인하는 클래스
+          decoration: BoxDecoration(
+            color: Colors.orange, // 색상
+          ),
+          child: Center(
+            child: Text('Splash Screen'),
+          ),
+        ),
+      ),
+    );
+  }
 }
