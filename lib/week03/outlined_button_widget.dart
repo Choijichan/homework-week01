bd6fbc8d730f4411f8584188a5c0db1be5768551
choijichan <sjy08007@gmail.com>
2025-09-19 12:53:58 +0900

    Fix: Corrected missing parentheses in OutlinedButton widget example

--- lib/week03/outlined_button_widget.dart
+++ lib/week03/outlined_button_widget.dart
@@ -11,12 +11,17 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: OutlineButton(
-  // 클릭 시 실행할 함수
-  onPressed: () {},
-  // 버튼 스타일
-  style: OutlinedButton.styleFrom(
-    foregroundColor: Colors.red,
-  ),
-  // 버튼에 들어갈 위젯
-  child: Text('아웃라인드 버튼'),
-)
+          // 클릭 시 실행할 함수
+          onPressed: () {},
+          // 버튼 스타일
+          style: OutlinedButton.styleFrom(
+            foregroundColor: Colors.red,
+          ),
+          // 버튼에 들어갈 위젯
+          child: Text('아웃라인드 버튼'),
+        )
+       )
+      )
+    )
+  }
+}
