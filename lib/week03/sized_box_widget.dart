65ffbd77b6d80679bf41721401ddd71283e095b8
choijichan <sjy08007@gmail.com>
2025-09-19 12:10:28 +0900

    feat: Implement SizedBox widget and add padding widget file

---
+++ lib/week03/sized_box_widget.dart
@@ -0,0 +1,13 @@
+SizedBox(
+  // 높이 지정
+  height: 200.0,
+
+  // 너비 지정
+  width: 200.0,
+
+  // SizedBox는 색상이 없으므로 크기를 확인하는
+  // 용도로 Container 추가
+  child: Container(
+    color: Colors.red,
+  ),
+)
