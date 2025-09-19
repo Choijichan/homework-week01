d6c31d0fcdc7457b788aa20bdc0eb7fe5a51cb95
choijichan <sjy08007@gmail.com>
2025-09-19 11:48:57 +0900

    feat: Add OutlinedButton widget to week03

---
+++ lib/week03/outlined_button_widget.dart
@@ -0,0 +1,10 @@
+OutlineButton(
+  // 클릭 시 실행할 함수
+  onPressed: () {},
+  // 버튼 스타일
+  style: OutlinedButton.styleFrom(
+    foregroundColor: Colors.red,
+  ),
+  // 버튼에 들어갈 위젯
+  child: Text('아웃라인드 버튼'),
+)
