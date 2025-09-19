bcdc9869a282735df66adcfb6ce8d8d9a3b4b427
choijichan <sjy08007@gmail.com>
2025-09-19 13:05:59 +0900

    feat: Implement basic Padding and Margin Widget structure

--- lib/week03/padding_margin_widget.dart
+++ lib/week03/padding_margin_widget.dart
@@ -1,1 +1,18 @@
-// 최상위 검정 컨테이너
+import'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child:
+        )
+      )
+    )
+  }
+}
