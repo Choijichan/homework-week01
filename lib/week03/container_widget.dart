f91323f706f8aab87f098c635d0eb075c7703b57
choijichan <sjy08007@gmail.com>
2025-09-19 12:20:58 +0900

    feat(container_widget): Add basic Container with BoxDecoration example.

--- lib/week03/container_widget.dart
+++ lib/week03/container_widget.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(FloatingActionButtonExample());
-}
 
-class FloatingActionButtonExample extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          // 클릭했을 때 실행할 함수
-          onPressed: () {},
-          child: Text('클릭'),
-        )
-      )
-    )
-  }
-}
+
 Container(
   // 스타일 작용
   decoration: BoxDecoration(
